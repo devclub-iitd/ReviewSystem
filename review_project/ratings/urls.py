33f8f1c7975cb4fbe6ae4887940b65b6f29489d0
DivyanshuSaxena <dssaxena2011@gmail.com>
2018-04-09 17:55:21 +0530

    Views added, checking remains

--- review_project/ratings/urls.py
+++ review_project/ratings/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     # path('', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^$', views.IndexView.as_view(), name='index')
 ]
--- review_project/ratings/views.py
+++ review_project/ratings/views.py
@@ -2,11 +2,41 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
 from django.views import generic
 from django.views.generic import View
+from . import models
+from . import forms
 
 # Create your views here.
 class IndexView(generic.ListView):
-    template_name = 'ratings/index.html'
+    #if logged in, display the current user's details
+    if request.session['user_id']:
+        template_name = 'ratings/user.html'
+        current_user = User.objects.get(userid=request.session['user_id'])        
+        return render(request, template_name, {'user':current_user})
+    #if not logged in redirect to url(/login)
+    else:
+        return redirect('ratings:login')
 
-class UserCreate(CreateView):
-    model = User
-    fields = ['']
+class LoginView(View):
+    form_class = LoginForm
+    template_name = 'ratings/login.html'
+    # Add user id to session variables
+    def login(request):
+        request.session['user_id'] = self.form_class.get_user_id()
+
+class LogoutView(View):
+    del request.session['user_id']
+    return redirect('ratings:user_list')
+
+class UserUpdate(UpdateView):
+    model = models.User
+    fields = ['name','about','updated_at']
+
+class WorkUpdate(UpdateView):
+    model = models.Work
+    fields = ['user','work']
+
+class UserDetailView(generic.DetailView):
+    template_name = 'ratings/user.html'
+    # Get ratings for this user, rated by the session user
+    # Edit the user details if the user id of the current view is the same as the session user
+    # Edit the work details if the user id of the current view is the same as the session user
