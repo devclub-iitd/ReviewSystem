7b7d02af46f0263cf56112840f214b375353e335
udit01 <uditjainiitd@gmail.com>
2018-04-09 17:59:23 +0530

    added static css and js files and added some urls

--- review_project/ratings/urls.py
+++ review_project/ratings/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     # path('', views.index, name='index'),
+    url(r'login/$', views.LoginView.as_view() , name='login'),
+    url(r'user_list/$', views.UserListView.as_view(), name='user_list')
+    
 ]
