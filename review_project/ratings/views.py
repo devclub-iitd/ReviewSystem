7d65df99067404f35e3316f457f73064e241ecee
DivyanshuSaxena <dssaxena2011@gmail.com>
2018-04-09 22:08:07 +0530

    View for User Details completed

--- review_project/ratings/views.py
+++ review_project/ratings/views.py
@@ -18,9 +18,6 @@ class IndexView(generic.ListView):
             except ObjectDoesNotExist:
                 return render(request, 'ratings/error.html',{'error': "The User for user_id : "+request.session['user_id']+" DoesNotExist. "})
 
-            # if current_user == None :
-            #     return render(request, 'ratings/error.html',{'error': "User not found: "+ request.session.user_id})
-                
             return render(request, template_name, {'user':current_user , 'current':True})
         #if not logged in redirect to url(/login)
         else:
@@ -40,7 +37,6 @@ class LoginView(View):
         if form.is_valid() :
             form.save()
             request.session['user_id'] = form.cleaned_data['userid']
-            
             return redirect('ratings:index')
 
 class LogoutView(View):
@@ -57,14 +53,28 @@ class WorkUpdate(generic.UpdateView):
     fields = ['user','work']
 
 class UserDetailView(generic.DetailView):
-     def get(self, request):
-        template_name = 'ratings/user.html'
-        try: 
-            user = models.User.objects.get(userid=request.GET.get('user','None'))     
-        except ObjectDoesNotExist:
-                return render(request, 'ratings/error.html',{'error': "The User for user_id : "+request.session['user_id']+" DoesNotExist. "})
-        return render(request, template_name, {'user':user , 'current':False})
-        #if not logged in redirect to url(/login)
+    def get(self, request):
+        if 'user_id' in request.session:
+            template_name = 'ratings/user.html'
+            try: 
+                user = models.User.objects.get(userid=request.GET.get('user','None'))
+                ratings = models.Rating.objects.all().filter(user1=request.session['user_id']).filter(user2=user).order_by('-updated_at')
+                current_rating = ratings[0].rating
+                works = models.Work.objects.all().filter(user=user).order_by('-updated_at')
+            except ObjectDoesNotExist:
+                return render(request, 'ratings/error.html',{'error': "The User for user_id : "+request.session['user_id']+" does not exist."})
+            return render(request, template_name, {'user':user, 'current':False, 'current_rated':current_rating, 'works':works})
+        else:
+            # if not logged in redirect to url(/login)
+            return redirect('ratings:login')  
     # Get ratings for this user, rated by the session user
     # Edit the user details if the user id of the current view is the same as the session user
     # Edit the work details if the user id of the current view is the same as the session user
+
+    # Function to check if the user id of the current view is the same as the session user
+    def is_same_user(self,request):
+        if 'user_id' in request.session:
+            user = models.User.objects.get(userid=request.GET.get('user','None'))
+            return (user.userid==request.session['user_id'])
+        else:
+            return False  
