c1c8b473ce85953e2c5fad4ef31b9649c375baec
DivyanshuSaxena <dssaxena2011@gmail.com>
2018-04-15 03:24:17 +0530

    Admin and superuser also working

--- review_project/ratings/admin.py
+++ review_project/ratings/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Profile
+from .models import Profile,Work,Rating
 
 # Register your models here.
 admin.site.register(Profile, UserAdmin)
+admin.site.register(Work)
+admin.site.register(Rating)
