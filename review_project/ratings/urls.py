d6a4a8eba7c4cc6aa87490e450e8ff12d9bb2466
Navneel <navneel99@gmail.com>
2018-06-13 21:40:48 +0530

    Login and Logout buttons appear as they should.

--- review_project/ratings/urls.py
+++ review_project/ratings/urls.py
@@ -8,7 +8,7 @@ app_name = 'ratings'
 
 urlpatterns = [
     path('', login_required(views.IndexView.as_view(template_name="ratings/user.html")), name='index'),
-    url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html', 'extra_context':{'type':'Login'}}, name='login'),
+    url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html', 'extra_context':{'type':'Login','logged_in':False}}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^register/$', views.RegisterView.as_view() , name='register'),
     url(r'^user/(?P<uid>\w+)/$',views.UserDetailView.as_view() , name='detail'),
