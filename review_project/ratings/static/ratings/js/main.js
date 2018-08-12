79b28395a29404ee1a4ef28bee6b99032feecbd0
Navneel <navneel99@gmail.com>
2018-08-12 18:23:59 +0530

    long works show up in 'alerts' in the Leaderboard.

--- review_project/ratings/static/ratings/js/main.js
+++ review_project/ratings/static/ratings/js/main.js
@@ -1,18 +1,29 @@
 $(document).ready(function(){
-    console.log("yay"); 
+    console.log("yay");
     $("input").addClass("form-control");
     $(".edit-form").hide();
     $(".exit").hide();
+    $(".actual_work").hide()
     $(".editable").on("click",function(){
         $(".edit-form").show();
-        $(".no-edit").hide();    
+        $(".no-edit").hide();
         $(".exit").show();
-        $(".editable").hide();        
+        $(".editable").hide();
     });
     $(".exit").on("click",function(){
         $(".edit-form").hide();
-        $(".no-edit").show();    
+        $(".no-edit").show();
         $(".editable").show();
         $(".exit").hide();
     })
+    $(".long_work").on("click",function(){
+      alert($(".actual_work").text());
+    });
+    $(".long_work").mouseover(function(){
+      $(this).css('color','blue');
+      $(this).css('cursor','pointer');
+    });
+    $(".long_work").mouseout(function(){
+      $(this).css('color','black');
+    });
 });
