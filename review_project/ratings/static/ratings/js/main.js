6ad1b3bbde1f835dfd0d40d338f13ca51e217807
Navneel <navneel99@gmail.com>
2018-08-27 20:17:33 +0530

    Alerts bug fixed.

--- review_project/ratings/static/ratings/js/main.js
+++ review_project/ratings/static/ratings/js/main.js
@@ -16,17 +16,18 @@ $(document).ready(function(){
         $(".editable").show();
         $(".exit").hide();
     })
-    
+
     $(".long_work").on("click",function(){
-      $(".actual_work").show();
-      $(".long_work").hide();
+      //$(".actual_work").show();
+      //$(".long_work").hide();
+      alert($(".actual_work").text());
     });
 
-    $(".actual_work").on("click",function(){
-      $(".actual_work").hide();
-      $(".long_work").show();
-    });
-    
+    //$(".actual_work").on("click",function(){
+      //$(".actual_work").hide();
+      //$(".long_work").show();
+    //});
+
     $(".long_work").mouseover(function(){
       $(this).css('color','blue');
       $(this).css('cursor','pointer');
